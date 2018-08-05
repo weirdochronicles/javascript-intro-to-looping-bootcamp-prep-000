80c9b8d985b04855cb64bb569b7d81e43dcd7049
Emma Goodman <emmagoodmannyc@gmail.com>
2018-08-05 22:19:59 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,26 +1,28 @@
 function forLoop(array){
-  for (let i = 0; i < 25; i++){
+  for(let i = 0; i < 25; i++){
     if (i === 1){
-    array.push(`I am ${i} strange loop.`)
+      array.push(`I am ${i} strange array.`)
     } else {
-      array.push(`I am ${i} strange loops.`)
+      array.push(`I am ${i} strange arrays.`)
     }
   }
   return array
 }
 
 function whileLoop(n){
-  while (n > 0){
+  while(n > 0){
     console.log(--n)
   }
-  return 'done'
+  return `done`
 }
 
 function doWhileLoop(array){
   function maybeTrue() {
   return Math.random() >= 0.5
-} do {
+}
+do{
   array = array.slice(1)
-} while (array.length > 0 && maybeTrue());
+} while(array.length > 0 && maybeTrue());
+
 return array
 }
